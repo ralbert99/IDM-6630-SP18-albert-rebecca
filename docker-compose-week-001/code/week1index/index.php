1013af608df78107b45a73bc52094b9d864ecbe5
ralbert99 <33538199+ralbert99@users.noreply.github.com>
2018-01-28 23:26:19 -0600

    Week 2 Final Commit

---
+++ docker-compose-week-001/code/week1index/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Snoopy: The Iconic Deuteragonist</title>
+  </head>
+  <body>
+    <?php
+      include "include/nav.php";
+      print "<h1>Snoopy: The Iconic Deuteragonist</h1>";
+    ?>
+  </body>
+</html>
