7bf87661ab46390801fd511eb1459f445273ebbb
ralbert99 <33538199+ralbert99@users.noreply.github.com>
2018-01-28 20:54:30 -0600

    Commit 2 Assignment 2

---
+++ code/about/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>About The Iconic Deuteragonist</title>
+  </head>
+  <body>
+    <?php
+      include "../include/nav.php";
+      print "<h1>About the Red Baron's Arch Nemesis</h1>";
+      echo "<p>Snoopy started out as nothing more than a dream in the fountain pen of Charles Schulz and turned into a loving companion that captured the hearts of thousands across the nation.</p>";
+      echo "<p>Many know Snoopy for his elaborate piloting scenes facing the Red Baron, or his witty quotes that leave readers thinking. 'There's no sense in doing a lot of barking if you don't really have anything to say.''</p>";
+    ?>
+  </body>
+</html>
---
+++ code/contact/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Contact Snoopy</title>
+  </head>
+  <body>
+    <?php
+      include "../include/nav.php";
+      print "<h1>Looking for Contact?</h1>";
+      echo "<p>Due to his fictional condition, getting in contact with the Charlie Brown's furry company isn't the easiest of tasks. However, we believe Mr. Shulz would join us in believing that Snoopy is within all of the hearts of Peanuts Cartoon readers, and you can often find him nestled alongside your childhood imagination, soaring through the cloads on his red doghouse.</p>";
+    ?>
+  </body>
+</html>
